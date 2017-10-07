dc0db9768f2ff5c57166bd789eb8098afd6eaef9
Sheyin <sheyinkage@gmail.com>
2017-10-07 00:56:38 -0500

    First commit

---
+++ Reversi/GameBoard.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+
+public class GameBoard
+{
+    // This defines the game board object
+    const int ROWS = 8;
+    const int COLS = 8;
+    char[,] board;
+
+    // Is a constructor needed?
+    public GameBoard()
+    {
+        board = new char[ROWS, COLS];
+
+        // initialize board to have all blank spaces
+        for (int i = 0; i < COLS; i++)
+        {
+            for (int j = 0; j < ROWS; j++)
+            {
+                board[i, j] = ' ';
+            }
+        }
+    }
+
+    // Display the game board in the console.
+	public void print()
+	{
+        // print column numbers first
+        Console.Write("  ");
+        for (int k = 1; k <= COLS; k++)
+        {
+            Console.Write(" " + k + " ");
+        }
+        Console.Write('\n');
+
+        // print the rest of the board data
+        for (int i = 0; i < COLS; i++)
+        {
+            Console.Write(i + 1 + " ");
+            for(int j = 0; j < ROWS; j++)
+            {
+                Console.Write("[" + board[i, j] + "]");
+            }
+            Console.Write('\n');
+        }
+     
+	}
+
+    // Returns true or false depending on whether the move was actually made.
+    // May want to move error message to the function calling the move.
+    public bool placePiece(char color, int row, int col)
+    {
+        // Check if it is a legal move (an opposing piece must be taken)
+        if (isLegalMove(color, row, col))
+        {
+            board[row, col] = color;
+            return true;
+        }
+
+        else
+        {
+            Console.WriteLine("Error: This is an illegal move.  Please pick a new location.");
+            return false;
+        }
+    }
+
+    bool isLegalMove(char newPiece, int row, int col)
+    {
+        // Check if position is blank / unclaimed
+        if (this.board[row, col] != ' ') {
+            return false;
+        }
+
+        // Check if position has an opposing piece adjacent to it
+        string[] directions = { "topLeft", "topCenter", "topRight", "rightCenter", "bottomRight", "bottomCenter", "bottomLeft", "leftCenter" };
+
+        bool matchFound = false;
+
+        for (int i = 0; i < 8; i++)
+        {
+            if (checkDirection(newPiece, row, col, directions[i]))
+            {
+                return true;
+            }
+        }
+
+        // No adjacent pieces that can be flipped in any direction, so it is illegal.
+        return false;  
+    }
+
+    // Makes an arrayList of pieces starting from the one next to origin and continuing in a given direction (not including origin)
+    // newRow, newCol: Starting point for array[0]
+    // rowModifier, colModifier: How much row / col should be increased or decreased until hits border
+    ArrayList makeArrayOfLine(int newRow, int newCol, int rowModifier, int colModifier)
+    {
+        ArrayList array = new ArrayList();
+
+        while (newRow >= 0 && newCol >= 0 && newRow < ROWS && newCol < COLS)
+        {
+            newRow += rowModifier;
+            newCol += colModifier;
+            array.Add(board[newRow, newCol]);
+        }
+
+        return array;
+    }
+
+    bool checkDirection(char newPiece, int newRow, int newCol, string direction)
+    {
+        ArrayList lineArray;
+        switch(direction)
+        {
+            case "topLeft":
+                lineArray = makeArrayOfLine(newRow, newCol, -1, -1);
+                return checkArrayForColorLine(newPiece, lineArray);
+                break;
+            case "topCenter":
+                lineArray = makeArrayOfLine(newRow, newCol, -1, 0);
+                return checkArrayForColorLine(newPiece, lineArray);
+                break;
+            case "topRight":
+                lineArray = makeArrayOfLine(newRow, newCol, -1, 1);
+                return checkArrayForColorLine(newPiece, lineArray);
+                break;
+            case "rightCenter":
+                lineArray = makeArrayOfLine(newRow, newCol, 0, 1);
+                return checkArrayForColorLine(newPiece, lineArray);
+                break;
+            case "bottomRight":
+                lineArray = makeArrayOfLine(newRow, newCol, 1, 1);
+                return checkArrayForColorLine(newPiece, lineArray);
+                break;
+            case "bottomCenter":
+                lineArray = makeArrayOfLine(newRow, newCol, 1, 0);
+                return checkArrayForColorLine(newPiece, lineArray);
+                break;
+            case "bottomLeft":
+                lineArray = makeArrayOfLine(newRow, newCol, 1, -1);
+                return checkArrayForColorLine(newPiece, lineArray);
+                break;
+            case "leftCenter":
+                lineArray = makeArrayOfLine(newRow, newCol, 0, -1);
+                return checkArrayForColorLine(newPiece, lineArray);
+                break;
+            default:
+                Console.WriteLine("Error: unidentified direction.");
+                return false;
+                break;
+        }
+    }
+
+    bool checkArrayForColorLine(char terminatingColor, ArrayList array)
+    {
+        int numOpposingColor = 0;
+        for (int i = 0; i < array.Count; i++)
+        {
+            if (array[i].Equals(terminatingColor))
+            {
+                if (numOpposingColor == 0)
+                {
+                    return false;
+                }
+
+                else
+                {
+                    return true;
+                }
+            }
+
+            else if (array[i].Equals(' ')) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+
+}
---
+++ Reversi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Reversi
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a game of Reversi (Othello).");
+
+            // encapsulate in while loop
+            GameBoard game = new GameBoard();
+            game.print();
+
+            // While loop this
+            Console.Write("For the space you want to move to, enter the row: ");
+            ConsoleKeyInfo positionRow = Console.ReadKey();
+            Console.Write('\n');
+            Console.WriteLine("You pressed: " + positionRow.KeyChar);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
